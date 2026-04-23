aaa3059685248b20261f9118a123b9ce91d7268c
geom3trik <geomyles@yahoo.co.uk>
2026-04-23 20:19:14 +0000

    Deploying to gh-pages from @ vizia/vizia-book@3aabc95f50946dddff8aeb2c028aee6c72314e67 🚀

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cea51ab7.js',
+                path_to_root + 'searchindex-501d1511.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
